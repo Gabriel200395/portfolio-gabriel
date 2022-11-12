1cf33093c0366d66c5efdb76f8e718d614d02234
Gabriel Souza <eugabrielf@gmail.com>
2022-11-12 19:01:11 -0300

    desenvolvendo pagina

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'react-circular-progressbar/dist/styles.css';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
