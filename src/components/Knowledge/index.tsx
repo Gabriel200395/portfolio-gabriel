e6ed750863a489d256b88268f35ae0ed5388ea03
Gabriel Souza <eugabrielf@gmail.com>
2022-11-22 12:36:20 -0300

    refatorando componente de conhecimentos

--- src/components/Knowledge/index.tsx
+++ src/components/Knowledge/index.tsx
@@ -1,179 +1,31 @@
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import { skills } from "../../helpers/arrays";
 import "./styles.css";
 
 const Knowledge = () => {
+
   return (
     <div className="knowledge-c" id="conhecimentos">
       <h2>Conhecimentos</h2>
       <div className="grid-skills">
-        <div className="card-skill">
-          <h3>HTML</h3>
-          <div className="progress">
-            <CircularProgressbar
-              value={100}
-              text={`${100}%`}
-              styles={buildStyles({
-                textColor: "#d1d1d1",
-                pathColor: "#047c3f",
-                trailColor: "#fff",
-              })}
-            />
-          </div>
-        </div>
-        <div className="card-skill">
-          <h3>Css</h3>
-          <div className="progress">
-            <CircularProgressbar
-              value={80}
-              text={`${80}%`}
-              styles={buildStyles({
-                textColor: "#d1d1d1",
-                pathColor: "#047c3f",
-                trailColor: "#fff",
-              })}
-            />
-          </div>
-        </div>
-        <div className="card-skill">
-          <h3>Bootstrap</h3>
-          <div className="progress">
-            <CircularProgressbar
-              value={70}
-              text={`70%`}
-              styles={buildStyles({
-                textColor: "#d1d1d1",
-                pathColor: "#047c3f",
-                trailColor: "#fff",
-              })}
-            />
-          </div>
-        </div>
-        <div className="card-skill">
-          <h3>JS</h3>
-          <div className="progress">
-            <CircularProgressbar
-              value={80}
-              text={`80%`}
-              styles={buildStyles({
-                textColor: "#d1d1d1",
-                pathColor: "#047c3f",
-                trailColor: "#fff",
-              })}
-            />
-          </div>
-        </div>
-        <div className="card-skill">
-          <h3>React JS</h3>
-          <div className="progress">
-            <CircularProgressbar
-              value={80}
-              text={`80%`}
-              styles={buildStyles({
-                textColor: "#d1d1d1",
-                pathColor: "#047c3f",
-                trailColor: "#fff",
-              })}
-            />
-          </div>
-        </div>
-        <div className="card-skill">
-          <h3>React Native</h3>
-          <div className="progress">
-            <CircularProgressbar
-              value={60}
-              text={`60%`}
-              styles={buildStyles({
-                textColor: "#d1d1d1",
-                pathColor: "#047c3f",
-                trailColor: "#fff",
-              })}
-            />
-          </div>
-        </div>
-        <div className="card-skill">
-          <h3>Typescript</h3>
-          <div className="progress">
-            <CircularProgressbar
-              value={80}
-              text={`80%`}
-              styles={buildStyles({
-                textColor: "#d1d1d1",
-                pathColor: "#047c3f",
-                trailColor: "#fff",
-              })}
-            />
-          </div>
-        </div>
-        <div className="card-skill">
-          <h3>Redux</h3>
-          <div className="progress">
-            <CircularProgressbar
-              value={80}
-              text={`80%`}
-              styles={buildStyles({
-                textColor: "#d1d1d1",
-                pathColor: "#047c3f",
-                trailColor: "#fff",
-              })}
-            />
-          </div>
-        </div>
-        <div className="card-skill">
-          <h3>Node JS</h3>
-          <div className="progress">
-            <CircularProgressbar
-              value={60}
-              text={`60%`}
-              styles={buildStyles({
-                textColor: "#d1d1d1",
-                pathColor: "#047c3f",
-                trailColor: "#fff",
-              })}
-            />
-          </div>
-        </div>
-        <div className="card-skill">
-          <h3>Express</h3>
-          <div className="progress">
-            <CircularProgressbar
-              value={60}
-              text={`60%`}
-              styles={buildStyles({
-                textColor: "#d1d1d1",
-                pathColor: "#047c3f",
-                trailColor: "#fff",
-              })}
-            />
-          </div>
-        </div>
-        <div className="card-skill">
-          <h3>Mysql</h3>
-          <div className="progress">
-            <CircularProgressbar
-              value={40}
-              text={`40%`}
-              styles={buildStyles({
-                textColor: "#d1d1d1",
-                pathColor: "#047c3f",
-                trailColor: "#fff",
-              })}
-            />
-          </div>
-        </div>
-        <div className="card-skill">
-          <h3>GitHub</h3>
-          <div className="progress">
-            <CircularProgressbar
-              value={60}
-              text={`60%`}
-              styles={buildStyles({
-                textColor: "#d1d1d1",
-                pathColor: "#047c3f",
-                trailColor: "#fff",
-              })}
-            />
-          </div>
-        </div>
+        {skills.map((skill, index) => {
+          return (
+            <div className="card-skill" key={index}>
+              <h3>{skill.name}</h3>
+              <div className="progress">
+                <CircularProgressbar
+                  value={skill.value}
+                  text={`${skill.value}%`}
+                  styles={buildStyles({
+                    textColor: "#d1d1d1",
+                    pathColor: "#047c3f",
+                    trailColor: "#fff",
+                  })}
+                />
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/helpers/arrays/index.ts
@@ -0,0 +1,55 @@
+type instanceSkill = {
+  name: string;
+  value: number;
+};
+
+export const skills: instanceSkill[] = [
+  {
+    name: "HTML",
+    value: 100,
+  },
+  {
+    name: "Css",
+    value: 70,
+  },
+  {
+    name: "Bootstrap",
+    value: 60,
+  },
+  {
+    name: "JS",
+    value: 80,
+  },
+  {
+    name: "React JS",
+    value: 80,
+  },
+  {
+    name: "React Native",
+    value: 60,
+  },
+  {
+    name: "Typescript",
+    value: 60,
+  },
+  {
+    name: "Redux",
+    value: 70,
+  },
+  {
+    name: "Node JS",
+    value: 80,
+  },
+  {
+    name: "Express",
+    value: 80,
+  },
+  {
+    name: "Mysql",
+    value: 40,
+  },
+  {
+    name: "GitHub",
+    value: 40,
+  },
+];
