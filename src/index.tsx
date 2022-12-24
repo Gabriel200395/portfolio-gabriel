506a8711cd89141f2628159485d306a31313f168
Gabriel Souza <eugabrielf@gmail.com>
2022-12-24 16:51:05 -0300

    arrumando link

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./page/App";
+import App from "./Page/App";
 import "react-circular-progressbar/dist/styles.css";
 
 const root = ReactDOM.createRoot(
